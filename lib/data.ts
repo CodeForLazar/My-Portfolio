9ae2e20c5bf7993319c2c255a57efc85376eb203
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2025-02-28 20:04:36 +0100

    update

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,6 @@
 import {createElement} from 'react';
 import {CgWorkAlt} from 'react-icons/cg';
 import {FaReact} from 'react-icons/fa';
-import {LuGraduationCap} from 'react-icons/lu';
 import chatBot from '@/public/chatBot.png';
 import rmtdevImg from '@/public/rmtdev.png';
 import wordanalyticsImg from '@/public/wordanalytics.png';
@@ -36,36 +35,36 @@ export type SectionName = (typeof links)[number]['name'];
 
 export const experiencesData = [
    {
+      title: 'Full-stack Developer',
+      location: 'Identry - Netherlands',
+      description:
+         'Developing an advanced Self-Sovereign Identity (SSI) solution powered by blockchain technology.',
+      icon: createElement(FaReact),
+      date: '2025 - Present',
+   },
+   {
+      title: 'Front-end Developer',
+      location: 'Dalmoregroup - USA',
+      description:
+         'Develop a fintech client-side application for a stock investment platform.',
+      icon: createElement(FaReact),
+      date: '2024 - 2025',
+   },
+   {
       title: 'Front-end Developer',
       location: 'Enterprise League - Skopje',
       description:
-         'I am currently working for Enterprise League a startup company that provides a B2B platform.',
+         'Worked for Enterprise League a startup company that provides a B2B platform.',
       icon: createElement(FaReact),
-      date: '2024 - Present',
+      date: '2023 - 2024',
    },
    {
       title: 'full-stack Developer',
       location: 'Undertone (for Perion) - Tel Aviv',
       description:
-         'Worked for Perion a  technology company that provides digital advertising products and services.',
+         'Worked for Perion a technology company that provides digital advertising products and services.',
       icon: createElement(FaReact),
-      date: '2021 - 2024',
-   },
-   {
-      title: 'Graduated Academy',
-      location: 'Semos Computer Education Center - Skopje',
-      description:
-         'I graduated after 9 months of studying. I immediately found a job as a full-stack developer.',
-      icon: createElement(LuGraduationCap),
-      date: '2020 - 2021',
-   },
-   {
-      title: 'Sales & Business development',
-      location: 'Evgeni - Skopje',
-      description:
-         'Represent companies and promote their varieties on a national level, Implementing effective marketing strategies, Increase sales and grow the customer base. ',
-      icon: createElement(CgWorkAlt),
-      date: '2016 - 2021',
+      date: '2021 - 2023',
    },
 ] as const;
 
@@ -115,5 +114,7 @@ export const skillsData = [
    'Redux',
    'Framer Motion',
    'TanStack Table',
+   'TanStack Query',
    'Moment.js',
+   'Date-fns.js',
 ] as const;
