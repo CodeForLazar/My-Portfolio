5f71c5c02ff4d9cc5fb5ed4515058ff705c976e3
Lazar Kiridjievski <l.kiridzievski@gmail.com>
2026-04-19 21:12:59 +0200

    add cv

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -93,7 +93,7 @@ const Intro = () => {
             </Link>
             <a
                className='borderBlack group flex cursor-pointer items-center gap-2 rounded-full bg-white px-7 py-3 outline-none transition hover:scale-110 focus:scale-110 active:scale-105 dark:bg-white/10'
-               href='/CV.pdf'
+               href='/CV_online.pdf'
                target='_blank'
             >
                Download CV{' '}
